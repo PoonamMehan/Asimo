7339fa569efff420c65f384b3e41baee60701964
PoonamMehan <poonammehan655@gmail.com>
2025-04-02 23:46:58 +0530

    Served client's code as a static folder

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,14 @@ import dotenv from "dotenv";
 import express from "express";
 import {router as chatRouter} from "./routes/chat.route.js"
 import cors from "cors"
+import path from 'path'
+import {fileURLToPath} from 'url';
 
 dotenv.config({
     path: "./.env" 
 })
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express();
 
@@ -17,6 +20,12 @@ app.use(cors({
 app.use(express.json( { limit : "1mb" } ))
 app.use(express.urlencoded({extended : true}))
 app.use("/api/v1/chat", chatRouter)
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+);
 
 
 
